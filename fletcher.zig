d0487b4929288da0415ff5582c8eb20b2e81874d
Léon van der Kaap <leonkaap@gmail.com>
2021-08-28 18:31:58 +0200

    Added feeding bytestreams into Fletcher32.

--- fletcher.zig
+++ fletcher.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const testing = std.testing;
 const assert = @import("std").debug.assert;
+const mem = std.mem;
 
 const MAX_BYTE = 255;
 const MAX_SHORT = 65535;
@@ -20,7 +21,7 @@ const Fletcher16 = struct {
         self.c1 = 0;
     }
 
-    pub fn update(self: *Self, input: []u8) void {
+    pub fn update(self: *Self, input: []const u8) void {
         for (input) |byte| {
             self.c0 = (self.c0 +% byte) % MAX_BYTE;
             self.c1 = (self.c1 +% self.c0) % MAX_BYTE;
@@ -35,25 +36,79 @@ const Fletcher16 = struct {
 const Fletcher32 = struct {
     c0: u32,
     c1: u32,
+    cache: u8,
+    has_cache: bool,
     const Self = @This();
 
     pub fn init() Self {
-        return Self{ .c0 = 0, .c1 = 0 };
+        return Self{ .c0 = 0, .c1 = 0, .cache = 0, .has_cache = false };
     }
 
     pub fn reset(self: *Self) void {
         self.c0 = 0;
         self.c1 = 0;
+        self.cache = 0;
+        self.has_cache = false;
     }
 
-    pub fn update(self: *Self, input: []u16) void {
-        for (input) |word| {
-            self.c0 = (self.c0 +% word) % MAX_SHORT;
-            self.c1 = (self.c1 +% self.c0) % MAX_SHORT;
+    pub fn update(self: *Self, input: []const u8) void {
+        if (input.len == 0) {
+            return;
+        } else if (input.len == 1) {
+            if (self.has_cache) {
+                const reconstructed = self.cache | @as(u16, input[0]) << 8;
+                self.update_internal_unit(reconstructed);
+                self.has_cache = false;
+            } else {
+                self.cache = input[0];
+                self.has_cache = true;
+            }
+        } else {
+            if (self.has_cache) {
+                const reconstructed = self.cache | @as(u16, input[0]) << 8;
+                self.update_internal_unit(reconstructed);
+                if (input.len % 2 != 0) {
+                    // Even number of elements left, no cache left
+                    const cast_array = mem.bytesAsSlice(u16, input[1..]);
+                    for (cast_array) |word| {
+                        self.update_internal_unit(word);
+                    }
+                    self.has_cache = false;
+                } else {
+                    // Odd number of elements left, put last element in cache
+                    const cast_array = mem.bytesAsSlice(u16, input[1..(input.len-1)]);
+                    for (cast_array) |word| {
+                        self.update_internal_unit(word);
+                    }
+                    self.cache = input[input.len - 1];
+                    self.has_cache = true;
+                }
+
+            } else {
+                const take = (input.len/2)*2;
+                var slice = input[0..take];
+                const cast_array = mem.bytesAsSlice(u16, slice);
+                for (cast_array) |word| {
+                   self.update_internal_unit(word);
+                }
+                if (take != input.len) {
+                    self.has_cache = true;
+                    self.cache = input[input.len - 1];
+                }
+            }
         }
     }
 
+    fn update_internal_unit(self: *Self, unit: u16) void {
+       self.c0 = (self.c0 +% unit) % MAX_SHORT;
+       self.c1 = (self.c1 +% self.c0) % MAX_SHORT;
+    }
+
     pub fn final(self: *Self) u32 {
+        if (self.has_cache) {
+            const reconstructed = @as(u16, self.cache);
+            self.update_internal_unit(reconstructed);
+        }
         return self.c1 << 16 | self.c0;
     }
 };
@@ -95,72 +150,144 @@ test "fletcher 16 empty string" {
 
 test "fletcher 16 string1" {
     const mystring_const: []const u8 = "abcde";
-    var mystring: [mystring_const.len]u8 = undefined;
-    for (mystring_const) |elem, i| {
-        mystring[i] = elem;
-    }
 
     var fletcher16 = Fletcher16.init();
-    fletcher16.update(mystring[0..]);
+    fletcher16.update(mystring_const[0..]);
     const result = fletcher16.final();
+
     const expected: u16 = 51440;
     try testing.expectEqual(expected, result);
 }
 
 test "fletcher 16 string2" {
     const mystring_const: []const u8 = "abcdef";
-    var mystring: [mystring_const.len]u8 = undefined;
-    for (mystring_const) |elem, i| {
-        mystring[i] = elem;
-    }
 
     var fletcher16 = Fletcher16.init();
-    fletcher16.update(mystring[0..]);
+    fletcher16.update(mystring_const[0..]);
     const result = fletcher16.final();
+
     const expected: u16 = 8279;
     try testing.expectEqual(expected, result);
 }
 
 test "fletcher 16 string3" {
     const mystring_const: []const u8 = "abcdefgh";
-    var mystring: [mystring_const.len]u8 = undefined;
-    for (mystring_const) |elem, i| {
-        mystring[i] = elem;
-    }
 
     var fletcher16 = Fletcher16.init();
-    fletcher16.update(mystring[0..]);
+    fletcher16.update(mystring_const[0..]);
     const result = fletcher16.final();
+
     const expected: u16 = 1575;
     try testing.expectEqual(expected, result);
 }
 
 test "fletcher 16 string3 piecewise" {
     const mystring_const1: []const u8 = "abc";
-    var mystring1: [mystring_const1.len]u8 = undefined;
-    for (mystring_const1) |elem, i| {
-        mystring1[i] = elem;
-    }
-
     const mystring_const2: []const u8 = "defgh";
-    var mystring2: [mystring_const2.len]u8 = undefined;
-    for (mystring_const2) |elem, i| {
-        mystring2[i] = elem;
-    }
 
     var fletcher16 = Fletcher16.init();
-    fletcher16.update(mystring1[0..]);
-    fletcher16.update(mystring2[0..]);
+    fletcher16.update(mystring_const1[0..]);
+    fletcher16.update(mystring_const2[0..]);
     const result = fletcher16.final();
+
     const expected: u16 = 1575;
     try testing.expectEqual(expected, result);
 }
 
 test "fletcher 32 empty string" {
-    var empty_block: []u16 = &.{};
+    var empty_block: []u8 = &.{};
     var fletcher32 = Fletcher32.init();
     fletcher32.update(empty_block);
     const result = fletcher32.final();
+
     const expected: u32 = 0;
     try testing.expectEqual(expected, result);
 }
+
+test "fletcher 32 string1" {
+    const mystring_const: []const u8 = "abcde";
+
+    var fletcher = Fletcher32.init();
+    fletcher.update(mystring_const[0..]);
+    const result = fletcher.final();
+
+    const expected: u32 = 4031760169;
+    try testing.expectEqual(expected, result);
+}
+
+test "fletcher 32 string2" {
+    const mystring_const: []const u8 = "abcdef";
+
+    var fletcher = Fletcher32.init();
+    fletcher.update(mystring_const[0..]);
+    const result = fletcher.final();
+
+    const expected: u32 = 1448095018;
+    try testing.expectEqual(expected, result);
+}
+
+test "fletcher 32 string3" {
+    const mystring_const: []const u8 = "abcdefgh";
+
+    var fletcher = Fletcher32.init();
+    fletcher.update(mystring_const[0..]);
+    const result = fletcher.final();
+
+    const expected: u32 = 3957429649;
+    try testing.expectEqual(expected, result);
+}
+
+test "fletcher 32 piecewise 1" {
+    const mystring_const1: []const u8 = "abc";
+    const mystring_const2: []const u8 = "defgh";
+
+    var fletcher = Fletcher32.init();
+    fletcher.update(mystring_const1[0..]);
+    fletcher.update(mystring_const2[0..]);
+    const result = fletcher.final();
+
+    const expected: u32 = 3957429649;
+    try testing.expectEqual(expected, result);
+}
+
+test "fletcher 32 piecewise 2" {
+    const mystring_const1: []const u8 = "abcd";
+    const mystring_const2: []const u8 = "efgh";
+
+    var fletcher = Fletcher32.init();
+    fletcher.update(mystring_const1[0..]);
+    fletcher.update(mystring_const2[0..]);
+    const result = fletcher.final();
+
+    const expected: u32 = 3957429649;
+    try testing.expectEqual(expected, result);
+}
+
+test "fletcher 32 piecewise 3" {
+    const mystring_const1: []const u8 = "abcdefg";
+    const mystring_const2: []const u8 = "h";
+
+    var fletcher = Fletcher32.init();
+    fletcher.update(mystring_const1[0..]);
+    try testing.expectEqual(true, fletcher.has_cache);
+    try testing.expectEqual(@as(u8, 'g'), fletcher.cache);
+    fletcher.update(mystring_const2[0..]);
+    const result = fletcher.final();
+
+    const expected: u32 = 3957429649;
+    try testing.expectEqual(expected, result);
+}
+
+test "fletcher 32 piecewise 4" {
+    const mystring_const1: []const u8 = "abcd";
+    const mystring_const2: []const u8 = "e";
+
+    var fletcher = Fletcher32.init();
+    fletcher.update(mystring_const1[0..]);
+    fletcher.update(mystring_const2[0..]);
+    const result = fletcher.final();
+
+    const expected: u32 = 4031760169;
+    try testing.expectEqual(expected, result);
+}
+
